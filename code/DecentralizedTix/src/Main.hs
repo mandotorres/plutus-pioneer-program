bf9b48718ca62c9648eef30e27b9403a446b74a4
A Torres <atorres@thecodeofarms.com>
2023-04-05 04:49:48 +0000

    save signed policy via main

--- code/DecentralizedTix/src/Main.hs
+++ code/DecentralizedTix/src/Main.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import           Signed                   (saveSignedPolicy)
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
+  saveSignedPolicy "71f450399d81967b635c7d3807017e21892cd79377a85a07d1e8eb82"
