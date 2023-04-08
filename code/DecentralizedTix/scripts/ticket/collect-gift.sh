6a3da6d0a8a77db38c71342d51e102967b47baae
A Torres <atorres@thecodeofarms.com>
2023-04-08 06:09:05 +0000

    collect nft from script address

--- code/DecentralizedTix/scripts/ticket/collect-gift.sh
+++ code/DecentralizedTix/scripts/ticket/collect-gift.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-assets=assets
-keypath=keys
 name="$1"
 collateral="$2"
 txin="$3"
+creationNFT="$4"
 
-pp="$assets/protocol.params"
-body="$assets/collect-thirtyfivetyped.raw"
-tx="$assets/collect-thirtyfivetyped.signed"
+pp="assets/protocol.params"
+body="assets/collect-thirtyfivetyped.raw"
+tx="assets/collect-thirtyfivetyped.signed"
+
+ADA="2"
+AMOUNT_LOVELACE=$(($ADA*1000000))
+SENDER_ADDR=$(cat keys/$1.addr)
+TOKEN_NAME=$(echo -n "$5" | xxd -ps | tr -d '\n')
+POLICY_ID="policy/policy-id-$creationNFT-$TOKEN_NAME"
 
 # Query the protocol parameters \
 
@@ -21,18 +26,20 @@ cardano-cli transaction build \
     --babbage-era \
     --testnet-magic 2 \
     --tx-in "$txin" \
-    --tx-in-script-file "$assets/thirtyfivetyped.plutus" \
+    --tx-in-script-file "assets/thirtyfivetyped.plutus" \
     --tx-in-inline-datum-present \
-    --tx-in-redeemer-file "$assets/value35.json" \
+    --tx-in-redeemer-file "assets/value35.json" \
     --tx-in-collateral "$collateral" \
-    --change-address "$(cat "$keypath/$name.addr")" \
+    --tx-out "$SENDER_ADDR+$AMOUNT_LOVELACE + 1 $(cat $POLICY_ID).$TOKEN_NAME" \
+    --change-address "$(cat "keys/$name.addr")" \
     --protocol-params-file "$pp" \
     --out-file "$body"
-    
+
+
 # Sign the transaction
 cardano-cli transaction sign \
     --tx-body-file "$body" \
-    --signing-key-file "$keypath/$name.skey" \
+    --signing-key-file "keys/$name.skey" \
     --testnet-magic 2 \
     --out-file "$tx"
 
