3cea845315ab7133fc91d7ef6044b9aeb19c55b8
A Torres <atorres@thecodeofarms.com>
2023-04-10 03:19:12 +0000

    clean up

--- code/DecentralizedTix/scripts/ticket/mint.sh
+++ code/DecentralizedTix/scripts/ticket/mint.sh
@@ -5,12 +5,8 @@
 # $2 = token name
 # $3 = utxoIn
 # $4 = collateral
-# $5 = utxoNFT
-# $6 = requiredUTXO
-# $7 = requiredTokenName
 
-
-ADA="10"
+ADA="2"
 AMOUNT_LOVELACE=$(($ADA*1000000))
 COLLATERAL_PKH=$(cat keys/$1.pkh)
 # INCORRECT_SIGNING_KEY="keys/user2.skey"
@@ -19,16 +15,16 @@ REDEEMER="assets/unit.json"
 SCRIPT_ADDR="assets/thirtyfivetyped.addr"
 SENDER_ADDR=$(cat keys/$1.addr)
 SENDER_SIGNING_KEY="keys/$1.skey"
-TOKEN_NAME=$(echo -n "$2" | xxd -ps | tr -d '\n')
-TOKEN_NAME_REQUIRED=$(echo -n "$7" | xxd -ps | tr -d '\n')
+TICKET_TOKEN_NAME=$(echo -n "$2" | xxd -ps | tr -d '\n')
+TC_TOKEN_NAME=$(echo -n "$5" | xxd -ps | tr -d '\n')
 
 # file outputs
-MINT_SCRIPT="assets/ticket-nft-$3-$TOKEN_NAME.plutus"
-POLICY_ID="policy/policy-id-$3-$TOKEN_NAME"
-# POLICY_ID_REQUIRED="policy/policy-id-$6-$TOKEN_NAME_REQUIRED"
+MINT_SCRIPT="assets/ticket-nft-$3-$TICKET_TOKEN_NAME.plutus"
+TICKET_POLICY_ID="policy/ticket-nft-$3-$TICKET_TOKEN_NAME"
+TC_POLICY_ID="policy/tc-nft-$6-$TC_TOKEN_NAME"
 PROTOCOL_PARAMS="assets/protocol.params"
-SIGNED_OUTPUT="assets/ticket-mint-tx-$3-$TOKEN_NAME.signed"
-UNSIGNED_OUTPUT="assets/ticket-mint-tx-$3-$TOKEN_NAME.raw"
+SIGNED_OUTPUT="assets/ticket-mint-tx-$3-$TICKET_TOKEN_NAME.signed"
+UNSIGNED_OUTPUT="assets/ticket-mint-tx-$3-$TICKET_TOKEN_NAME.raw"
 
 # generate protocol params every time the script is run
 cardano-cli query protocol-parameters $NETWORK --out-file $PROTOCOL_PARAMS
@@ -39,8 +35,8 @@ if [ ! -d "policy" ]; then
 fi
 
 # generate policy id every time the script is run
-cardano-cli transaction policyid --script-file $MINT_SCRIPT > $POLICY_ID
-echo -e "policy id file $POLICY_ID created\n"
+cardano-cli transaction policyid --script-file $MINT_SCRIPT > $TICKET_POLICY_ID
+echo -e "policy id file $TICKET_POLICY_ID created\n"
 
 # echo "sender: $1"
 # echo "token name: $2"
@@ -52,38 +48,31 @@ echo -e "policy id file $POLICY_ID created\n"
 # echo "collateral pkh: $COLLATERAL_PKH"
 # echo "minting script: $MINT_SCRIPT"
 # echo "network: $NETWORK"
-# echo "policy id: $POLICY_ID"
+# echo "policy id: $TICKET_POLICY_ID"
 # echo "protocol params: $PROTOCOL_PARAMS"
 # echo "redeemer: $REDEEMER"
 # echo "sender addr: $SENDER_ADDR"
 # echo "sender signing key: $SENDER_SIGNING_KEY"
 # echo "signed tx file: $SIGNED_OUTPUT"
-# echo "token name: $TOKEN_NAME"
+# echo "token name: $TICKET_TOKEN_NAME"
 # echo -e "unsigned tx file: $UNSIGNED_OUTPUT\n"
 
-# echo "$SENDER_ADDR+$AMOUNT_LOVELACE + 1 $(cat $POLICY_ID).$TOKEN_NAME"
-# echo "--tx-out $SENDER_ADDR+$AMOUNT_LOVELACE+1 $(cat $POLICY_ID).$TOKEN_NAME"
+# echo "$SENDER_ADDR+$AMOUNT_LOVELACE + 1 $(cat $TICKET_POLICY_ID).$TICKET_TOKEN_NAME"
+# echo "--tx-out $SENDER_ADDR+$AMOUNT_LOVELACE+1 $(cat $TICKET_POLICY_ID).$TICKET_TOKEN_NAME"
 
-# Build thirtyfivetyped address 
-cardano-cli address build \
-    --payment-script-file "assets/thirtyfivetyped.plutus" \
-    $NETWORK \
-    --out-file $SCRIPT_ADDR
-
-
-  # --tx-out "$(cat $SCRIPT_ADDR) + 1 $(cat $POLICY_ID_REQUIRED).$TOKEN_NAME_REQUIRED" \
-
+# --tx-in "8bad8b001474817b3dffd7cdf6022dcc4b0fa34d62412a9cb802f136f4a83a95#1" \
 cardano-cli transaction build \
   --babbage-era \
   $NETWORK \
   --tx-in $3 \
-  --tx-in $5 \
+  --tx-in $7 \
   --required-signer-hash $COLLATERAL_PKH \
   --tx-in-collateral $4 \
-  --tx-out "$SENDER_ADDR+$AMOUNT_LOVELACE + 1 $(cat $POLICY_ID).$TOKEN_NAME" \
+  --tx-out "$(cat $SCRIPT_ADDR)+$AMOUNT_LOVELACE + 1 $(cat $TICKET_POLICY_ID).$TICKET_TOKEN_NAME" \
   --tx-out-inline-datum-file "assets/unit.json" \
+  --tx-out "$SENDER_ADDR+$AMOUNT_LOVELACE + 1 $(cat $TC_POLICY_ID).$TC_TOKEN_NAME" \
   --change-address $SENDER_ADDR \
-  --mint "1 $(cat $POLICY_ID).$TOKEN_NAME" \
+  --mint "1 $(cat $TICKET_POLICY_ID).$TICKET_TOKEN_NAME" \
   --mint-script-file $MINT_SCRIPT \
   --mint-redeemer-file $REDEEMER \
   --protocol-params-file $PROTOCOL_PARAMS \
@@ -108,4 +97,4 @@ cardano-cli transaction build \
 #   --tx-file $SIGNED_OUTPUT
 
 # TX_ID=$(cardano-cli transaction txid --tx-file $SIGNED_OUTPUT)
-echo -e "\nTransaction txid: https://preview.cexplorer.io/tx/$TX_ID\n"
+# echo -e "\nTransaction txid: https://preview.cexplorer.io/tx/$TX_ID\n"
