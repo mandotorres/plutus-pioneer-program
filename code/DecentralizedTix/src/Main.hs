3db0c1cfeb8537124a95ca551665fb11ac0082a7
A Torres <atorres@thecodeofarms.com>
2023-04-14 02:27:56 +0000

    Add User contract

--- code/DecentralizedTix/src/Main.hs
+++ code/DecentralizedTix/src/Main.hs
@@ -2,9 +2,9 @@
 
 module Main where
 
-import           Signed                   (saveSignedPolicy)
+import           User                   (saveUserPolicy)
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  saveSignedPolicy "71f450399d81967b635c7d3807017e21892cd79377a85a07d1e8eb82"
+  saveUserPolicy "71f450399d81967b635c7d3807017e21892cd79377a85a07d1e8eb82"
---
+++ code/DecentralizedTix/src/User.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+module User where
+
+import           Plutus.V2.Ledger.Api      (BuiltinData, CurrencySymbol,
+                                            MintingPolicy, PubKeyHash,
+                                            ScriptContext (scriptContextTxInfo),
+                                            mkMintingPolicyScript)
+import           Plutus.V2.Ledger.Contexts (txSignedBy)
+import qualified PlutusTx
+import           PlutusTx.Prelude          (Bool, traceIfFalse, ($), (.))
+import           Prelude                   (IO, Show (show))
+import           Text.Printf               (printf)
+import           Utilities                 (currencySymbol, wrapPolicy, writePolicyToFile)
+
+{-# INLINABLE mkUserPolicy #-}
+mkUserPolicy :: PubKeyHash -> () -> ScriptContext -> Bool
+mkUserPolicy pkh () ctx = traceIfFalse "missing signature" $ txSignedBy (scriptContextTxInfo ctx) pkh
+
+{-# INLINABLE mkWrappedUserPolicy #-}
+mkWrappedUserPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkWrappedUserPolicy pkh = wrapPolicy (mkUserPolicy $ PlutusTx.unsafeFromBuiltinData pkh)
+
+userCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> ())
+userCode = $$(PlutusTx.compile [|| mkWrappedUserPolicy ||])
+
+userPolicy :: PubKeyHash -> MintingPolicy
+userPolicy pkh = mkMintingPolicyScript $ userCode `PlutusTx.applyCode` PlutusTx.liftCode (PlutusTx.toBuiltinData pkh)
+
+---------------------------------------------------------------------------------------------------
+------------------------------------- HELPER FUNCTIONS --------------------------------------------
+
+saveUserPolicy :: PubKeyHash -> IO ()
+saveUserPolicy pkh = writePolicyToFile (printf "assets/user-mint-policy-%s.plutus" $ show pkh) $ userPolicy pkh
+
+userCurrencySymbol :: PubKeyHash -> CurrencySymbol
+userCurrencySymbol = currencySymbol . userPolicy
