63edc45c263d94fc79a497c1a9a0c5557a7e612a
A Torres <atorres@thecodeofarms.com>
2023-04-16 04:14:50 +0000

    Add user token validation and update scripts

--- code/DecentralizedTix/scripts/vesting/grab.sh
+++ code/DecentralizedTix/scripts/vesting/grab.sh
@@ -4,11 +4,20 @@ USER=$1
 COLLATERAL=$2
 VESTED_UTXO=$3
 SLOT=$4
+PAYMENT_UTXO=$5
+USER_TOKEN_UTXO=$6
 
-BENEFICIARY_PKH=$(cat keys/company.pkh)
+BENEFICIARY=company
+BENEFICIARY_ADDR=$(cat keys/$BENEFICIARY.addr)
+BENEFICIARY_PKH=$(cat keys/$BENEFICIARY.pkh)
+BENEFICIARY_SIGNING_KEY=keys/$BENEFICIARY.skey
+COST=6000000 # 6 ada
+POLICY_ID="policy/user-$BENEFICIARY_PKH"
 PROTOCOL_PARAMS=assets/protocol.params
 SIGNED_OUTPUT=assets/collect-gift.signed
+TOKEN_NAME=$(echo -n "User" | xxd -ps | tr -d '\n')
 UNSIGNED_OUTPUT=assets/collect-gift.raw
+USER_ADDRESS=$(cat keys/$USER.addr)
 
 # Query the protocol parameters \
 
@@ -21,19 +30,24 @@ cardano-cli transaction build \
     --babbage-era \
     --testnet-magic 2 \
     --required-signer-hash $BENEFICIARY_PKH \
+    --tx-in $PAYMENT_UTXO \
+    --tx-in $USER_TOKEN_UTXO \
     --tx-in $VESTED_UTXO \
     --tx-in-script-file assets/parameterized-vesting.plutus \
     --tx-in-inline-datum-present \
     --tx-in-redeemer-file assets/unit.json \
     --tx-in-collateral $COLLATERAL \
+    --tx-out "$BENEFICIARY_ADDR + $COST" \
+    --tx-out "$USER_ADDRESS + 2000000 + 1 $(cat $POLICY_ID).$TOKEN_NAME " \
     --invalid-before $SLOT \
-    --change-address $(cat keys/$USER.addr) \
+    --change-address $USER_ADDRESS \
     --protocol-params-file $PROTOCOL_PARAMS \
     --out-file $UNSIGNED_OUTPUT
     
 # Sign the transaction
 cardano-cli transaction sign \
     --tx-body-file $UNSIGNED_OUTPUT \
+    --signing-key-file $BENEFICIARY_SIGNING_KEY \
     --signing-key-file keys/$USER.skey \
     --testnet-magic 2 \
     --out-file $SIGNED_OUTPUT
--- code/DecentralizedTix/src/PVesting.hs
+++ code/DecentralizedTix/src/PVesting.hs
@@ -8,14 +8,15 @@
 module PVesting where
 
 import           Plutus.V1.Ledger.Interval (contains)
-import           Plutus.V2.Ledger.Api      (BuiltinData, POSIXTime, PubKeyHash,
+import           Plutus.V1.Ledger.Value    (AssetClass, assetClass, assetClassValueOf)
+import           Plutus.V2.Ledger.Api      (adaSymbol, adaToken, BuiltinData, POSIXTime, PubKeyHash,
                                             ScriptContext (scriptContextTxInfo),
                                             TxInfo (txInfoValidRange),
                                             Validator, from, mkValidatorScript)
-import           Plutus.V2.Ledger.Contexts (txSignedBy)
+import           Plutus.V2.Ledger.Contexts (valuePaidTo, valueSpent)
 import           PlutusTx                  (applyCode, compile, liftCode,
                                             makeLift)
-import           PlutusTx.Prelude          (Bool, traceIfFalse, ($), (&&), (.))
+import           PlutusTx.Prelude          (Bool, traceIfFalse, ($), (&&), (==), (<=))
 import           Prelude                   (IO)
 import           Utilities                 (wrapValidator, writeValidatorToFile)
 
@@ -23,35 +24,43 @@ import           Utilities                 (wrapValidator, writeValidatorToFile)
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
 
 data VestingParams = VestingParams
-    { beneficiary :: PubKeyHash
+    { beneficiary :: PubKeyHash -- TODO: Possibly move to datum
     , deadline    :: POSIXTime
     }
 makeLift ''VestingParams
 
 {-# INLINABLE mkParameterizedVestingValidator #-}
-mkParameterizedVestingValidator :: VestingParams -> () -> () -> ScriptContext -> Bool
-mkParameterizedVestingValidator params () () ctx =
-    traceIfFalse "beneficiary's signature missing" signedByBeneficiary &&
-    traceIfFalse "deadline not reached" deadlineReached
+mkParameterizedVestingValidator :: AssetClass -> VestingParams -> () -> () -> ScriptContext -> Bool
+mkParameterizedVestingValidator ac params () () ctx =
+    traceIfFalse "missing user token"      hasUserToken    &&
+    traceIfFalse "price not paid"          pricePaid       &&
+    traceIfFalse "deadline not reached"    deadlineReached
+
   where
     info :: TxInfo
     info = scriptContextTxInfo ctx
 
-    signedByBeneficiary :: Bool
-    signedByBeneficiary = txSignedBy info $ beneficiary params
-
     deadlineReached :: Bool
     deadlineReached = contains (from $ deadline params) $ txInfoValidRange info
 
-{-# INLINABLE  mkWrappedParameterizedVestingValidator #-}
-mkWrappedParameterizedVestingValidator :: VestingParams -> BuiltinData -> BuiltinData -> BuiltinData -> ()
-mkWrappedParameterizedVestingValidator = wrapValidator . mkParameterizedVestingValidator
+    ada :: AssetClass
+    ada = assetClass adaSymbol adaToken
+    
+    pricePaid :: Bool
+    pricePaid = 6 <= assetClassValueOf (valuePaidTo info (beneficiary params)) ada
 
-validator :: VestingParams -> Validator
-validator params = mkValidatorScript ($$(compile [|| mkWrappedParameterizedVestingValidator ||]) `applyCode` liftCode params)
+    hasUserToken :: Bool
+    hasUserToken = 1 == assetClassValueOf (valueSpent info) ac
+
+{-# INLINABLE  mkWrappedParameterizedVestingValidator #-}
+mkWrappedParameterizedVestingValidator :: AssetClass -> VestingParams -> BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkWrappedParameterizedVestingValidator ac params = wrapValidator $ mkParameterizedVestingValidator ac params
+
+validator :: AssetClass -> VestingParams -> Validator
+validator ac params = mkValidatorScript ($$(compile [|| mkWrappedParameterizedVestingValidator ||]) `applyCode` liftCode ac `applyCode` liftCode params)
 
 ---------------------------------------------------------------------------------------------------
 ------------------------------------- HELPER FUNCTIONS --------------------------------------------
 
-saveVal :: VestingParams -> IO ()
-saveVal = writeValidatorToFile "./assets/parameterized-vesting.plutus" . validator
+saveVal :: AssetClass -> VestingParams -> IO ()
+saveVal ac params = writeValidatorToFile "./assets/parameterized-vesting.plutus" $ validator ac params
