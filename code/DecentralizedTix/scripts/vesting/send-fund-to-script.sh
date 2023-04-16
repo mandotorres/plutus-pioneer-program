44b5715e435a3a6efa7f35edc71abda913e19356
A Torres <atorres@thecodeofarms.com>
2023-04-16 22:31:51 +0000

    Send ticket back to script address

--- code/DecentralizedTix/scripts/vesting/send-fund-to-script.sh
+++ code/DecentralizedTix/scripts/vesting/send-fund-to-script.sh
@@ -3,20 +3,28 @@
 USER=$1
 PAYMENT_UTXO=$2
 TICKET_NFT_UTXO=$3
-MINT_UTXO=$4
+TC_NFT_UTXO=$4
+TC_MINT_UTXO=$5
 
 
 ADA="2"
 AMOUNT_LOVELACE=$(($ADA*1000000))
 SCRIPT_ADDR="assets/gift.addr"
-SENDER_ADDR=$(cat keys/$USER.addr)
-TOKEN_NAME=$(echo -n "Ticket" | xxd -ps | tr -d '\n')
-POLICY_ID="policy/ticket-nft-$MINT_UTXO-$TOKEN_NAME"
+TC_TOKEN_NAME=$(echo -n "Ticket Creator" | xxd -ps | tr -d '\n')
+TICKET_TOKEN_NAME=$(echo -n "Ticket" | xxd -ps | tr -d '\n')
+USER_ADDR=$(cat keys/$USER.addr)
+COMPANY_PKH=$(cat keys/company.pkh)
+USER_SIGNING_KEY="keys/$USER.skey"
 
+# file outputs
+TC_POLICY_ID=$(cat "policy/tc-nft-$COMPANY_PKH-$TC_MINT_UTXO-$TC_TOKEN_NAME")
+PARAMS_STRING="$TC_POLICY_ID-$TC_TOKEN_NAME-$TC_NFT_UTXO-$TICKET_TOKEN_NAME"
 NETWORK="--testnet-magic 2"
-SENDER_SIGNING_KEY="keys/$USER.skey"
-UNSIGNED_OUTPUT="assets/pv-$RECIPIENT_PKH-$POSIX_TIME.raw"
-SIGNED_OUTPUT="assets/pv-$RECIPIENT_PKH-$POSIX_TIME.signed"
+TICKET_POLICY_ID="policy/ticket-nft-$PARAMS_STRING"
+UNSIGNED_OUTPUT="assets/pv-$PARAMS_STRING.raw"
+SIGNED_OUTPUT="assets/pv-$PARAMS_STRING.signed"
+USER_POLICY_ID="policy/user-$COMPANY_PKH"
+USER_TOKEN_NAME=$(echo -n "User" | xxd -ps | tr -d '\n')
 
 # Build gift address 
 cardano-cli address build \
@@ -31,16 +39,16 @@ cardano-cli transaction build \
     $NETWORK \
     --tx-in $PAYMENT_UTXO \
     --tx-in $TICKET_NFT_UTXO \
-    --tx-out "$(cat $SCRIPT_ADDR)+$AMOUNT_LOVELACE + 1 $(cat $POLICY_ID).$TOKEN_NAME" \
+    --tx-out "$(cat $SCRIPT_ADDR)+$AMOUNT_LOVELACE + 1 $(cat $TICKET_POLICY_ID).$TICKET_TOKEN_NAME + 1 $(cat $USER_POLICY_ID).$USER_TOKEN_NAME" \
     --tx-out-inline-datum-file assets/unit.json \
-    --tx-out "$SENDER_ADDR+$AMOUNT_LOVELACE + 0 $(cat $POLICY_ID).$TOKEN_NAME" \
-    --change-address $SENDER_ADDR \
+    --tx-out "$USER_ADDR+$AMOUNT_LOVELACE + 0 $(cat $TICKET_POLICY_ID).$TICKET_TOKEN_NAME" \
+    --change-address $USER_ADDR \
     --out-file $UNSIGNED_OUTPUT
 
 # Sign the transaction
 cardano-cli transaction sign \
-    --tx-body-file "$UNSIGNED_OUTPUT" \
-    --signing-key-file $SENDER_SIGNING_KEY \
+    --tx-body-file $UNSIGNED_OUTPUT \
+    --signing-key-file $USER_SIGNING_KEY \
   $NETWORK \
   --out-file $SIGNED_OUTPUT
 
