a82cfb731bf2311331dfccd03da65dcde48deba2
A Torres <atorres@thecodeofarms.com>
2023-05-04 05:48:11 +0000

    Move params to datum, add price to datum

--- code/DecentralizedTix/src/ParameterizedVesting.hs
+++ code/DecentralizedTix/src/ParameterizedVesting.hs
@@ -7,75 +7,83 @@
 
 module ParameterizedVesting where
 
+import           Data.Maybe                (fromJust)
 import qualified Data.ByteString.Char8      as BS8
 import           Plutus.V1.Ledger.Interval  (contains)
 import           Plutus.V1.Ledger.Value     (AssetClass, assetClass, assetClassValueOf, unAssetClass)
-import           Plutus.V2.Ledger.Api       (adaSymbol, adaToken, BuiltinData, POSIXTime (getPOSIXTime), PubKeyHash,
-                                             ScriptContext (scriptContextTxInfo),
-                                             TokenName(unTokenName),
-                                             TxInfo (txInfoValidRange),
-                                             Validator, from, mkValidatorScript)
+import           Plutus.V2.Ledger.Api       (adaSymbol, adaToken, BuiltinData, POSIXTime, PubKeyHash
+                                           , ScriptContext (scriptContextTxInfo)
+                                           , TokenName(unTokenName)
+                                           , TxInfo (txInfoValidRange)
+                                           , Validator, from, mkValidatorScript)
 import           Plutus.V2.Ledger.Contexts  (valuePaidTo, valueSpent)
-import           PlutusTx                   (applyCode, compile, liftCode,
-                                             makeLift)
+import           PlutusTx                   (applyCode, compile, liftCode
+                                           , unstableMakeIsData)
 import           PlutusTx.Builtins.Internal (BuiltinByteString (BuiltinByteString))
 import           PlutusTx.Prelude           (Bool, fst, snd, traceIfFalse, ($), (&&), (==), (<=))
-import           Prelude                    (IO, Show (show), String)
+import           Prelude                    (IO, Show (show), String, Integer)
 import           Text.Printf                (printf)
-import           Utilities                  (bytesToHex, wrapValidator, writeValidatorToFile)
+import           Utilities                  (bytesToHex, posixTimeFromIso8601, printDataToJSON
+                                           , wrapValidator, writeValidatorToFile)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
 
-data VestingParams = VestingParams
-    { beneficiary :: PubKeyHash -- TODO: Possibly move to datum
-    , deadline    :: POSIXTime
+data VestingDatum = VestingDatum
+    { beneficiary      :: PubKeyHash
+    , dateAvailable    :: POSIXTime
+    , price            :: Integer
     }
-makeLift ''VestingParams
+unstableMakeIsData ''VestingDatum
 
 {-# INLINABLE mkParameterizedVestingValidator #-}
-mkParameterizedVestingValidator :: AssetClass -> VestingParams -> () -> () -> ScriptContext -> Bool
-mkParameterizedVestingValidator ac params () () ctx =
-    traceIfFalse "missing user token"      hasUserToken    &&
-    traceIfFalse "price not paid"          pricePaid       &&
-    traceIfFalse "deadline not reached"    deadlineReached
+mkParameterizedVestingValidator :: AssetClass -> VestingDatum -> () -> ScriptContext -> Bool
+mkParameterizedVestingValidator ac dat () ctx =
+    traceIfFalse "missing user token"              hasUserToken                 &&
+    traceIfFalse "beneficiary not paid in full"    beneficiaryPaid              &&
+    traceIfFalse "ticket is not yet available"     availableDateReached
 
   where
     info :: TxInfo
     info = scriptContextTxInfo ctx
 
-    deadlineReached :: Bool
-    deadlineReached = contains (from $ deadline params) $ txInfoValidRange info
+    availableDateReached :: Bool
+    availableDateReached = contains (from $ dateAvailable dat) $ txInfoValidRange info
 
     ada :: AssetClass
     ada = assetClass adaSymbol adaToken
-    
-    pricePaid :: Bool
-    pricePaid = 6 <= assetClassValueOf (valuePaidTo info (beneficiary params)) ada
+
+    beneficiaryPaid :: Bool
+    beneficiaryPaid = price dat <= assetClassValueOf (valuePaidTo info (beneficiary dat)) ada
 
     hasUserToken :: Bool
     hasUserToken = 1 == assetClassValueOf (valueSpent info) ac
 
 {-# INLINABLE  mkWrappedParameterizedVestingValidator #-}
-mkWrappedParameterizedVestingValidator :: AssetClass -> VestingParams -> BuiltinData -> BuiltinData -> BuiltinData -> ()
-mkWrappedParameterizedVestingValidator ac params = wrapValidator $ mkParameterizedVestingValidator ac params
+mkWrappedParameterizedVestingValidator :: AssetClass -> BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkWrappedParameterizedVestingValidator ac = wrapValidator $ mkParameterizedVestingValidator ac
 
-validator :: AssetClass -> VestingParams -> Validator
-validator ac params = mkValidatorScript ($$(compile [|| mkWrappedParameterizedVestingValidator ||]) `applyCode` liftCode ac `applyCode` liftCode params)
+validator :: AssetClass -> Validator
+validator ac = mkValidatorScript ($$(compile [|| mkWrappedParameterizedVestingValidator ||]) `applyCode` liftCode ac)
 
 ---------------------------------------------------------------------------------------------------
 ------------------------------------- HELPER FUNCTIONS --------------------------------------------
 
-saveVal :: AssetClass -> VestingParams -> IO ()
-saveVal ac params = writeValidatorToFile
-    (printf "./assets/parameterized-vesting-%s-%s-%s-%s.plutus"
+saveVal :: AssetClass -> IO ()
+saveVal ac = writeValidatorToFile
+    (printf "./assets/parameterized-vesting-%s-%s.plutus"
       (show (fst (unAssetClass ac)))
       tokenName
-      (show $ beneficiary params)
-      (show $ getPOSIXTime (deadline params))
-    ) $ 
-    validator ac params
+    ) $
+    validator ac
   where
     tokenName :: String
     tokenName = case unTokenName (snd (unAssetClass ac)) of
         (BuiltinByteString bs) -> BS8.unpack $ bytesToHex bs
+
+printVestingDatumJSON :: PubKeyHash -> String -> Integer -> IO ()
+printVestingDatumJSON pkh time cost = printDataToJSON $ VestingDatum
+    { beneficiary      = pkh
+    , dateAvailable    = fromJust $ posixTimeFromIso8601 time
+    , price            = cost
+    }
